5cde5ef1c4a2a9819e5263958831fb690ff77716
Fahad Aziz <fahad.mentorsol@gmail.com>
2025-07-08 19:33:47 +0500

    Implement Express server with CORS and cookie parsing; add ApiError and ApiResponse utility classes; include asyncHandler for error handling

--- src/index.js
+++ src/index.js
@@ -1,27 +1,17 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
 
-connectDB();
-
-// import express from "express";
-
-// const app = express();
-
-// (async () => {
-//   try {
-//     mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//     app.on("error", (error) => {
-//       console.error("Error connecting to MongoDB:", error);
-//     });
-//     app.listen(process.env.PORT, () => {
-//       console.log(`server is running on port ${process.env.PORT}`);
-//     });
-//   } catch (error) {
-//     console.error("Error connecting to MongoDB:", error);
-//     throw error;
-//   }
-// })();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to the database:", err);
+  });
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
+
+export { asyncHandler };
+
+// this is an higher order function that returns an async function
+
+// const asyncHandler = () => async (req, res, next) => {
+//   try {
+//   } catch (error) {
+//     res.status(error.status || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
