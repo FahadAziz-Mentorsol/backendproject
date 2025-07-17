adde4abe4674279eaa5aef3f5c3d2186a4e2c23f
Fahad Aziz <fahad.mentorsol@gmail.com>
2025-07-17 11:31:24 +0500

    Refactor app initialization to include dotenv configuration; remove unused import in user model

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
@@ -17,7 +19,6 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 //routes import
-
 import userRouter from "./routes/user.routes.js";
 
 app.use("/api/v1/users", userRouter);
